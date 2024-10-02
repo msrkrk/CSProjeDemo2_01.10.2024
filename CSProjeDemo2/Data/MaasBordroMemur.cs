878823ff03ca570bc5f86144064229febd9f27c5
Deniz TANIR <tanirdeniz35@gmail.com>
2024-10-02 12:49:45 +0300

    MaasBordro

---
+++ CSProjeDemo2/Data/MaasBordroMemur.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CSProjeDemo2.Data
+{
+    internal class MaasBordroMemur:MaasBordro
+    {
+        public string MesaiUcreti { get; set; }
+
+        public void BordroIcerikGetir(Memur personel)
+        {
+             
+            PersonelIsmi = personel.Name;
+            CalismaSaati = CalismaSaati;
+            AnaOdeme = personel.Maas.ToString();
+            MesaiUcreti = personel.ekMesaiUcret.ToString();
+            ToplamOdeme = personel.ToplamOdenenTutarGetir().ToString();
+
+        }
+    }
+}
